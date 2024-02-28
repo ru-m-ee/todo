9b03d32b538dca95fa6c87fe4da69cbe07fcec39
ru-m-ee <ru-m-ee@outlook.com>
2024-02-29 04:32:33 +0600

    CICD

--- backend/djangoproject/api/urls.py
+++ backend/djangoproject/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("list/", views.PlanList.as_view()),
     path("create/", views.PlanCreate.as_view()),
+    path("update/<int:id>/", views.PlanUpdate.as_view()),
     path("delete/<int:id>", views.PlanDestroy.as_view()),
 ]
